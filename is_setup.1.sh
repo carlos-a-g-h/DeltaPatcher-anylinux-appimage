62cf08764c25c1432777b99c01eb5f49d7acb8dd
carlos-a-g-h <carloselputohabanero@proton.me>
2026-05-19 19:26:07 -0400

    setup dot one with no config

--- is_setup.1.sh
+++ is_setup.1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#!/bin/bash
 
 # NOTE: THIS IS AN INTERNAL SCRIPT AND IT CAN ONLY RUN INSIDE THE APPIMAGE AS
 # A COMMAND LINE ARGUMENT
@@ -7,13 +8,16 @@ set -eu
 
 MAIN_BIN="/usr/bin/DeltaPatcher"
 
+CONFIG_DIR="/"
+
 DESKTOP="DeltaPatcher.desktop"
 DESKTOP_EXEC=$(basename "$MAIN_BIN")
+
 PATH_ICON="/usr/share/icons/DeltaPatcher.png"
 declare -a LBINARIES=(
 	"$MAIN_BIN"
 )
 
-#function additional_config_tasks() {
-#	# sed -i "s:HOME_DIRECTORY:$HOME:" "$CONFIG_DIR"/conf.txt
-#}
+function additional_config_tasks() {
+	echo "nothing"
+}
